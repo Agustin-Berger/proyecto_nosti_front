73a4e674be86c146ae230f8c66b1ce853f85ef4b
Agustin Berger <aguberger07@gmail.com>
2025-03-21 17:45:34 -0300

    toma del nuevo back

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configurar el cliente Axios
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://proyecto-nosti-back.onrender.com/",
   withCredentials: true,
 });
 
